7e14eb749a30c7dbdaad054ff324c33926b5bd66
GustavoFantin <gustavofantinbarros@gmail.com>
2024-12-01 22:39:59 -0800

    not finished yet, a lot problems with modals and accordion

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ $(function() {
   const user = await fetchUserById(userId)
   const post = await fetchPostsById(userId)
   const todo = await fetchTodosById(userId)
+
   displayData(user, post, todo)
   })  //next button
   
@@ -50,7 +51,6 @@ $(function() {
       type: 'GET',
       success: function(todoData) {  
         todo = todoData
-        console.log(todo)
       } // fetching the api and passing the ID number through const currUser
     })
     
@@ -64,15 +64,35 @@ $(function() {
       url: `https://dummyjson.com/users/${id}/posts`,
       type: 'GET',
       success: function(postData) {
-        console.log(postData, id)
         dataPosts = postData
+                
+        
+        dataPosts.posts.forEach(element => {
+          let modalId = element.id // passing ID for modal directly from post api
+          const modalTag = fetchModalById(modalId)
+        }) 
+
+        
+      },
+    })
+  }
+
+  const fetchModalById = (modalId) => {
+    let modalData
+    return $.ajax({
+      url: `https://dummyjson.com/posts/${modalId}`,
+      type: 'GET',
+      success: function(postModal) {
+         modalData = postModal
+         console.log("MODAL:", modalData)
       },
     })
   }
 
 
 
-  const displayData = (currUser, dataPosts, todo) => {
+
+  const displayData = (currUser, dataPosts, todo, modalData) => {
     
     $('.info__image img').attr("src", "" )
     $('.info__content').html("")
@@ -84,27 +104,47 @@ $(function() {
     $('.todos h3').html("")
 
 
-    console.log("data", dataPosts)
-
     $('.info__image img').attr("src", currUser.image )
     $('.info__content').append(`
-      <h1>${currUser.firstName} ${currUser.lastName}</h1>
-      <h3>Age: ${currUser.age}</h3>
-      <h3>Email: ${currUser.email}</h3>
-      <h3>Phone: ${currUser.phone}</h3>
+      <h2>${currUser.firstName} ${currUser.lastName}</h2>
+      <p><strong>Age:</strong> ${currUser.age}</p>
+      <p><strong>Email:</strong> ${currUser.email}</p>
+      <p><strong>Phone:</strong> ${currUser.phone}</p>
     `)
 
+    //posts
     $('.posts h3').append(`${currUser.firstName}'s Posts`)
 
     if (dataPosts.posts.length === 0) {
       $('.posts ul').append(`<li>User has no posts</li>`)
     } else {
       dataPosts.posts.forEach(element => {
-        // $('.posts ul').append(`<h4>${element.title}</h4>`)
         $('.posts ul').append(`<li><h4>${element.title}</h4>${element.body}</li>`)
+        
       }) 
     }
 
+    //modal
+    $('.posts').on('click', 'h4', async () => {
+      // const modal = await $(`
+      //   <div class="overlay">
+      //     <div class="modal">
+      //     </div>
+      //   </div>
+      //   `)
+      //   $('.modal').append(`
+      //     <h2>${modalData.title}</h2>
+      //     <p>${modalData.body}</h2>
+      //     <em>Views: ${modalData.views}</em>
+      //     <button>Close Modal</button>
+      //     `)
+      //     $('.container').append(modal)
+          
+    })
+
+    console.log(modalData.title)
+
+    //todos
     $('.todos h3').append(`${currUser.firstName}'s To Dos`)
     if(todo.todos.length === 0) {
       $('.todos ul').append(`<li>User has no to dos</li>`)
@@ -122,8 +162,8 @@ $(function() {
     const user = await fetchUserById(userId)
     const posts = await fetchPostsById(userId)
     const todo = await fetchTodosById(userId)
+
     displayData(user, posts, todo)
-    console.log("here",posts);
   }
 
   getData()
